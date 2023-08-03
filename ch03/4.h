e5c34c75823ca80bace7739a94178c77274a35ea
Mike Harris <MikeMKH@users.noreply.github.com>
2023-08-03 06:04:21 -0500

    init 3.4

---
+++ ch03/4.h
@@ -0,0 +1,15 @@
+/*
+ *	Header for calculator program
+ */
+
+#define NSYMS 20	/* maximum number of symbols */
+
+struct symtab {
+	char *name;
+	double value;
+} symtab[NSYMS];
+
+struct symtab *symlook();
+
+int yylex();
+void yyerror();
